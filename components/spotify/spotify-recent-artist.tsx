13e3f932de48f3ff653a684ef91482274a4289e4
Syauqy Nurul Aziz <syauqyaziz@gmail.com>
2021-08-11 13:02:08 +0700

    feat(ui): add pulse while fetching spotify data

--- components/spotify/spotify-recent-artist.tsx
+++ components/spotify/spotify-recent-artist.tsx
@@ -30,44 +30,50 @@ export default function SpotifyRecentArtist() {
 
   useEffect(() => {
     showTopArtists();
-    console.log(artists);
   }, []);
-
-  console.log(artists);
 
   return (
     <div className="p-4 pb-6 w-full bg-white bg-opacity-50 shadow-md rounded-md">
       <h2 className="text-center p-2 text-gray-700">
         Recently I'm listening to 🎸
       </h2>
-      <div className="flex flex-wrap justify-center">
-        {artists?.records
-          ? artists.records.map((artist, i: number) => (
-              <div key={i} className="p-2">
-                <div className="flex-shrink-0">
-                  <Link href={artist.fields.url} passHref>
-                    <a target="_blank" rel="nofollow noopener noreferrer">
-                      <Tippy
-                        className="rounded-md shadow-lg p-1 bg-gray-800 text-white"
-                        content={<span>{artist.fields.name}</span>}
-                        delay={100}
-                        placement="bottom"
-                        arrow={false}
-                        offset={[0, 5]}
-                      >
-                        <img
-                          className="h-20 w-20 object-cover shadow-md rounded-full transform hover:scale-110 hover:rotate-3"
-                          src={artist.fields.img_url}
-                          alt={artist.fields.name}
-                        />
-                      </Tippy>
-                    </a>
-                  </Link>
-                </div>
+
+      {artists?.records ? (
+        <div className="flex flex-wrap justify-center">
+          {artists.records.map((artist, i: number) => (
+            <div key={i} className="p-2">
+              <div className="flex-shrink-0">
+                <Link href={artist.fields.url} passHref>
+                  <a target="_blank" rel="nofollow noopener noreferrer">
+                    <Tippy
+                      className="rounded-md shadow-lg p-1 bg-gray-800 text-white"
+                      content={<span>{artist.fields.name}</span>}
+                      delay={100}
+                      placement="bottom"
+                      arrow={false}
+                      offset={[0, 5]}
+                    >
+                      <img
+                        className="h-20 w-20 object-cover shadow-md rounded-full transform hover:scale-110 hover:rotate-3"
+                        src={artist.fields.img_url}
+                        alt={artist.fields.name}
+                      />
+                    </Tippy>
+                  </a>
+                </Link>
               </div>
-            ))
-          : null}
-      </div>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className="animate-pulse flex flex-wrap justify-center">
+          <div className="h-20 w-20 bg-gray-400 rounded-full mx-2"></div>
+          <div className="h-20 w-20 bg-gray-400 rounded-full mx-2"></div>
+          <div className="h-20 w-20 bg-gray-400 rounded-full mx-2"></div>
+          <div className="h-20 w-20 bg-gray-400 rounded-full mx-2"></div>
+          <div className="h-20 w-20 bg-gray-400 rounded-full mx-2"></div>
+        </div>
+      )}
     </div>
   );
 }
